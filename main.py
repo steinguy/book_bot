28ea9b677b5dbb61132939cb24dc4599da7f0899
steinguy <michaelfsteinacker@gmail.com>
2026-01-10 11:05:29 -0500

    add char_count

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ def get_book_text(path):
         file_contents = f.read()
         return file_contents
 
-from stats import count_words
+from stats import count_words, count_characters
 
 def main():
     
     path = "books/frankenstein.txt"
     text = get_book_text(path)
-    num_words = count_words(path)
+    num_words = count_words(text)
+    char_count = count_characters(text)
     
     # print(text)
     print(f"Found {num_words} total words")
+    print(char_count)
     
 main()   
--- stats.py
+++ stats.py
@@ -1,11 +1,20 @@
-def count_words(path):
-    with open(path) as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        total_words = len(words)
-        return total_words
+def count_words(text):
+    words = text.split()
+    total_words = len(words)
+    return total_words
 
-def count_characters(path):
-    with open(path) as f:
-        file_contents = f.read()
+
+
+def count_characters(text):
+    char_count = {}
+    for char in text:
+        lowercase_char = char.lower()
+        if lowercase_char in char_count:
+            char_count[lowercase_char] += 1
+        else: 
+            char_count[lowercase_char] = 1
+    return char_count
+
+
+
         
