d9b11b5525fda2a13981c4ee793c83a7202df04c
steinguy <michaelfsteinacker@gmail.com>
2026-01-12 21:14:32 -0500

    add sorted report and arguments

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
+import sys
+
+def check_argv(args):
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return args[1]
+        
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
 
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sorted_list
+
+
 
 def main():
-    
-    path = "books/frankenstein.txt"
+    path = check_argv(sys.argv)
     text = get_book_text(path)
+
     num_words = count_words(text)
     char_count = count_characters(text)
+    sorted_chars = sorted_list(char_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_chars:
+        ch = item["char"]
+        if not ch.isalpha():
+            continue
+        print(f"{ch}: {item['num']}")
+
+    print("============= END ===============")
     
     # print(text)
-    print(f"Found {num_words} total words")
-    print(char_count)
+    #print(f"Found {num_words} total words")
+    #print(char_count)
     
-main()   
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -15,6 +15,20 @@ def count_characters(text):
             char_count[lowercase_char] = 1
     return char_count
 
+def sort_on(items):
+    return items["num"]
+
+def sorted_list(char_count):
+
+    items_list = []
+
+    for char, count in char_count.items():
+        items = {"char": char, "num": count}
+        items_list.append(items)
+    
+    items_list.sort(reverse=True, key=sort_on)
+    return items_list
+
 
 
         
